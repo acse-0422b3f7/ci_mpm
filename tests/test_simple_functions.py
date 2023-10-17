f2df16fe7eb7cd88b694e65777019900c9c9dbfa
acse-0422b3f7 <yue.wang423@imperial.ac.uk>
2023-10-17 17:27:22 +0100

    upload

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -15,7 +15,6 @@ class TestSimpleFunctions(object):
         isum = my_sum(iterable)
         assert isum == expected
 
-
     @pytest.mark.parametrize('number, expected', [
         (5, 120),
         (3, 6),
@@ -25,3 +24,4 @@ class TestSimpleFunctions(object):
         '''Test our factorial function'''
         answer = factorial(number)
         assert answer == expected
+        
